83a2d72d49145466ef06655421037bd8ec0e1d90
Jason Chae <jbro0921@gmail.com>
2023-03-13 10:05:11 -0400

    copy paste

--- src/main/java/vehicle/Car.java
+++ src/main/java/vehicle/Car.java
@@ -1,3 +1,20 @@
-public abstract class Car {
-    
-}
+@throws IllegalArgumentException if startingMileage is negative*/
+
+public Car(String make, String model, double startingMileage)
+
+public Car(String make, String model)
+
+@throws IllegalArgumentException 
+public boolean canDrive(double miles)
+@throws IllegalArgumentException 
+public abstract void drive(double miles)
+
+public String toString()
+public double getMileage()
+public String getMake()
+public String getModel()
+public abstract double getRemainingRange()
+@throws IllegalArgumentException 
+protected void addMileage(double miles);
+@throws IllegalArgumentException 
+public int roadTrip(List<Double> milesEachDay)
