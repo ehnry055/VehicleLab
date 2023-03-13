84c187b1f62266a970bdd21b827a3f9cee4ab378
ehnry055 <henry.g.choi@gmail.com>
2023-03-13 10:38:41 -0400

    created interfaces and ford

---
+++ src/main/java/vehicle/FordFrivolous.java
@@ -0,0 +1,12 @@
+package vehicle;
+
+public class FordFrivolous extends GasPoweredCar{
+
+    public FordFrivolous (double startingMileage) {
+        
+    }
+
+    public FordFrivolous() {
+        mlg = 0;
+    }
+}
