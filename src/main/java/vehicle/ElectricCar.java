796d8bde334e999526cbb7377aff1e42efd27dca
Muriel_Dayanna <98164830+daymuriel@users.noreply.github.com>
2023-03-14 08:15:53 -0400

    might work

--- src/main/java/vehicle/ElectricCar.java
+++ src/main/java/vehicle/ElectricCar.java
@@ -1,4 +1,71 @@
-public abstract class ElectricCar extends Car{
-    
+package vehicle;
+public abstract class ElectricCar extends Car {
+    double milesOnMaxCharge;
+    double charge;
+    /**
+     * Note: Car begins with a full charge (and thus range).
+     * 
+     * @throws IllegalArgumentException if milesOnMaxCharge is nonpositive.
+     */
+    public ElectricCar(String make, String model, double startingMileage, double milesOnMaxCharge) {
+        super(make, model, startingMileage);
+        this.milesOnMaxCharge = milesOnMaxCharge;
+        charge = milesOnMaxCharge;
+        if (milesOnMaxCharge < 0){
+            throw new IllegalArgumentException();
+        }
+
+
+    }
+
+    /**
+     * Defaults mileage to 0.
+     * 
+     * @throws IllegalArgumentException if milesOnMaxCharge is nonpositive.
+     */
+    public ElectricCar(String make, String model, double milesOnMaxCharge) {
+        super(make,model);
+        this.milesOnMaxCharge = milesOnMaxCharge;
+
+        
+    }
+
+    /**
+     * Drives the full given number of miles.
+     * 
+     * @throws IllegalArgumentException if miles is negative.
+     * @throws IllegalArgumentException if miles is too high given the current charge.
+     */
+    public void drive(double miles) {
+        if (miles < 0 || miles > getRemainingRange()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Returns how many more miles the car can currently go without
+     * recharging.
+     */
+    public double getRemainingRange() {
+        return charge;
+    }
+
+    /** Returns how many miles the car could go on a full charge. */
+    public double getMaxRange() {
+        return milesOnMaxCharge;
+    }
+
+    /** Recharges the car to max range capability. */
+    public void recharge() {
+        charge += (milesOnMaxCharge - charge);
+    }
+
+    /**
+     * Decreases the amount of energy in the battery based by the number
+     * of miles passed as an argument.
+     */
+    protected void decreaseCharge(double miles) {
+        charge = milesOnMaxCharge - miles;
+    }
 }
 
