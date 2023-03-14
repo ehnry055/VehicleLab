5f6ad4a7d26a58fbf7a233f435f060a299c5cec5
Muriel_Dayanna <98164830+daymuriel@users.noreply.github.com>
2023-03-14 10:41:02 -0400

    seperated interfaces

--- src/main/java/vehicle/SelfDriving.java
+++ src/main/java/vehicle/SelfDriving.java
@@ -1,18 +1,5 @@
 package vehicle;
 public interface SelfDriving {
-    public void driveAutonomously(double miles) {
-        
-    }
+    public void driveAutonomously(double miles);
 }
 
-interface Flying {
-    public boolean canFly(double miles) {
-        if (miles < 0) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    public void fly(double miles) {
-
-    }
-}
